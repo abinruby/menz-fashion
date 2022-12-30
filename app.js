74c1bec1b4d4b3bb208b3700c03516c459791911
ABIN K RUBY <107753584+abinruby@users.noreply.github.com>
2022-12-30 14:52:41 +0000

    atlas integrated

--- app.js
+++ app.js
@@ -62,7 +62,7 @@ app.use(function(err, req, res, next) {
    res.render('include/404',{adminlog,userlog})
 });
 
-mongoose.connect('mongodb+srv://abinkruby:abin12345678@cluster0.gqa7vyt.mongodb.net/?retryWrites=true&w=majority').then(()=>{console.log('Database connected Successfully');})
+mongoose.connect('mongodb+srv://abin123:<abin123>@cluster0.fzyoauk.mongodb.net/test').then(()=>{console.log('Database connected Successfully');})
 .catch((err)=>{console.log(err);})
 
 module.exports = app;
